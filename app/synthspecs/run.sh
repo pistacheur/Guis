4631715e510a3f7c5bcbbe9cb16692a8004363b0
pistacheur <pilotindzodjou.pd@gmail.com>
2021-08-08 19:43:35 +0200

    first commit

---
+++ app/synthspecs/run.sh
@@ -0,0 +1,1 @@
+../../../../tools/Orchestrator/orchestratorExplicit.py ../src/main/res/layout/activity_main.xml MainActivitySpecs.txt ../src/main/java/com/example/MainActivity.java --gameOutputFile game.txt
